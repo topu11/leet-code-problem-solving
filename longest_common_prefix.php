dfa386bf1d0aeca579abe22a82ff57c8001a0dff
encoderit-touhid <touhid@encoderit.net>
2025-06-13 14:41:21 +0600

    logest and reseve iniger

---
+++ longest_common_prefix.php
@@ -0,0 +1,19 @@
+<?php
+$strs=["flower","flow","flight"];
+var_dump(longest_common_subsequence($strs));
+function longest_common_subsequence($strs)
+{
+      $longest_common_subsequence="";
+      for($i=0;$i<strlen($strs[0]);$i++)
+      {
+          for($j=1;$j<count($strs);$j++)
+          {
+                if(!isset($strs[$j][$i]) || ($strs[0][$i] != $strs[$j][$i]))
+                {
+                    return $longest_common_subsequence;
+                }
+          }
+          $longest_common_subsequence=$longest_common_subsequence.$strs[0][$i];
+      }
+      return $longest_common_subsequence;
+}
--- reverse_intiger.php
+++ reverse_intiger.php
@@ -1,28 +1,26 @@
 <?php
-$input = 120536;
-echo 'input is: ' . $input;
-echo '<br>';
-echo 'output is: ' . reverse($input);
-
+$input = -120536;
+var_dump(reverse($input));
 function reverse($x)
 {
-    $flag = false;
-    if ($x < 0) {
-        $flag = true;
-        $x = $x * (-1);
+    $is_negative=false;
+    if($x < 0)
+    {
+        $is_negative=true;
+        $x=$x*(-1);
     }
 
     $reversedInt = 0;
     while ($x > 0) {
         $a = $x % 10;
-        $x = floor($x / 10);
+        $x = (int)($x / 10);
 
         //Should no larger than Math.floor(Number.MAX_VALUE/10)
         if ($reversedInt >= 214748365)
             return 0;
         $reversedInt = $reversedInt * 10 + $a;
     }
-    if ($flag) {
+    if ($is_negative) {
         $reversedInt = $reversedInt * (-1);
     }
     return $reversedInt;
